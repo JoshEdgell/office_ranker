03f3ccff896ce98614c323b3a761ae8e21a6428c
JoshEdgell <josh.0.edgell@gmail.com>
2020-01-15 20:19:54 -0500

    created server.js and basic folder structure

---
+++ server.js
@@ -0,0 +1,31 @@
+const express         = require('express');
+const bodyParser      = require('body-parser');
+const mongoose        = require('mongoose');
+const app             = express();
+const router          = express.Router();
+
+//Middleware
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+//Routers
+
+app.get('/', (req,res)=>{
+  res.render(index.html);
+});
+
+const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/officeranker'
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connection.once('open', ()=>{
+	console.log("That's what she said.");
+});
+
+const PORT = 3000;
+const port = process.env.PORT || PORT;
+
+app.listen(port, ()=>{
+  console.log("Dunder Mifflin, this is Pam.")
+  console.log("==========================PORT " + PORT + "==================================")
+});
